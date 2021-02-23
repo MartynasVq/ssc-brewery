4c9bde048815ac7a79ff83b61d4117b09449d0bb
Martynas <nbtsst@gmail.com>
2021-02-23 22:15:47 +0200

    Authentication principle

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerOrderControllerV2Test.java
@@ -0,0 +1,126 @@
+package guru.sfg.brewery.web.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.sfg.brewery.bootstrap.DefaultBreweryLoader;
+import guru.sfg.brewery.domain.Beer;
+import guru.sfg.brewery.domain.BeerOrder;
+import guru.sfg.brewery.domain.Customer;
+import guru.sfg.brewery.repositories.BeerOrderRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+class BeerOrderControllerV2Test extends BaseIT {
+    public static final String API_ROOT = "/api/v2/orders/";
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    Customer stPeteCustomer;
+    Customer dunedinCustomer;
+    Customer keyWestCustomer;
+    List<Beer> loadedBeers;
+
+    @BeforeEach
+    void setUp() {
+        stPeteCustomer = customerRepository.findAllByCustomerName(DefaultBreweryLoader.ST_PETE).orElseThrow();
+        dunedinCustomer = customerRepository.findAllByCustomerName(DefaultBreweryLoader.DUNEDIN).orElseThrow();
+        keyWestCustomer = customerRepository.findAllByCustomerName(DefaultBreweryLoader.KEY_WEST).orElseThrow();
+        loadedBeers = beerRepository.findAll();
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).apply(springSecurity()).build();
+    }
+
+    @Test
+    void listOrdersNotAuth() throws Exception {
+        mockMvc.perform(get(API_ROOT))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @WithUserDetails(value = "admin2")
+    @Test
+    void listOrdersAdminAuth() throws Exception {
+        mockMvc.perform(get(API_ROOT))
+                .andExpect(status().isOk());
+    }
+
+    @WithUserDetails("stpete")
+    @Test
+    void listOrdersCustomerAuth() throws Exception {
+        mockMvc.perform(get(API_ROOT))
+                .andExpect(status().isOk());
+    }
+
+    @WithUserDetails("dunedin")
+    @Test
+    void listOrdersCustomerDunedinAuth() throws Exception {
+        mockMvc.perform(get(API_ROOT ))
+                .andExpect(status().isOk());
+    }
+
+
+    
+    @Transactional
+    @Test
+    void getByOrderIdNotAuth() throws Exception {
+        BeerOrder beerOrder = stPeteCustomer.getBeerOrders().stream().findFirst().orElseThrow();
+
+        mockMvc.perform(get(API_ROOT + beerOrder.getId()))
+                .andExpect(status().isUnauthorized());
+    }
+
+    
+    @Transactional
+    @WithUserDetails("admin2")
+    @Test
+    void getByOrderIdADMIN() throws Exception {
+        BeerOrder beerOrder = stPeteCustomer.getBeerOrders().stream().findFirst().orElseThrow();
+
+        mockMvc.perform(get(API_ROOT + beerOrder.getId()))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    
+    @Transactional
+    @WithUserDetails("stpete")
+    @Test
+    void getByOrderIdCustomerAuth() throws Exception {
+        BeerOrder beerOrder = stPeteCustomer.getBeerOrders().stream().findFirst().orElseThrow();
+
+        mockMvc.perform(get(API_ROOT + beerOrder.getId()))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    
+    @Transactional
+    @WithUserDetails("dunedin")
+    @Test
+    void getByOrderIdCustomerNOTAuth() throws Exception {
+        BeerOrder beerOrder = stPeteCustomer.getBeerOrders().stream().findFirst().orElseThrow();
+
+        mockMvc.perform(get(API_ROOT + beerOrder.getId()))
+                .andExpect(status().isNotFound());
+    }
+}
