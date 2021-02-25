90e3a8db5aa800aea27cc966203cc89356182fc2
Martynas <nbtsst@gmail.com>
2021-02-25 18:52:19 +0200

    Locking account after 3 failed attempts

--- src/main/java/guru/sfg/brewery/security/listeners/AuthFailureListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthFailureListener.java
@@ -1,5 +1,10 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.seurity.LoginFailureRepository;
+import guru.sfg.brewery.repositories.seurity.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,25 +12,52 @@ import org.springframework.security.authentication.event.AuthenticationFailureBa
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.sql.Timestamp.valueOf;
+
+@RequiredArgsConstructor
 @Slf4j
 @Component
 public class AuthFailureListener {
+
+    private final LoginFailureRepository loginFailureRepository;
+    private final UserRepository userRepository;
 
     @EventListener
     public void listen(AuthenticationFailureBadCredentialsEvent event) {
         log.debug("Login failure");
 
         if(event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            LoginFailure.LoginFailureBuilder builder = LoginFailure.builder();
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
 
             if(token.getPrincipal() instanceof String) {
+                userRepository.findByUsername((String) token.getPrincipal()).ifPresent(builder::user);
                 log.debug("Failed authentication for user " + token.getPrincipal());
             }
             if(token.getDetails() instanceof WebAuthenticationDetails) {
                 WebAuthenticationDetails wad = (WebAuthenticationDetails) token.getDetails();
-
+                builder.sourceIp(wad.getRemoteAddress());
                 log.debug("IP: " + wad.getRemoteAddress());
             }
+
+            LoginFailure loginFailure = loginFailureRepository.save(builder.build());
+
+            if(loginFailure.getUser() != null)
+                lockUserAccount(loginFailure.getUser());
+        }
+    }
+
+    private void lockUserAccount(User user) {
+        List<LoginFailure> failures = loginFailureRepository.findAllByUserAndCreatedDateIsAfter(user,
+                valueOf(LocalDateTime.now().minusDays(1)));
+        if(failures.size() > 3) {
+            log.debug("Locking user account.");
+            user.setAccountNonLocked(false);
+            userRepository.save(user);
         }
     }
 }
