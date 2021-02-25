f558a1d527145aaaa31a1afd73230a2ff023117a
Martynas <58885736+MartynasVq@users.noreply.github.com>
2021-02-25 20:37:23 +0200

    Added google auth bean and controller functionality

--- src/main/java/guru/sfg/brewery/config/SecurityBeans.java
+++ src/main/java/guru/sfg/brewery/config/SecurityBeans.java
@@ -1,5 +1,8 @@
 package guru.sfg.brewery.config;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorConfig;
+import com.warrenstrange.googleauth.ICredentialRepository;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +12,7 @@ import org.springframework.security.web.authentication.rememberme.JdbcTokenRepos
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.sql.DataSource;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class SecurityBeans {
@@ -21,6 +25,21 @@ public class SecurityBeans {
     @Bean
     public AuthenticationEventPublisher authenticationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
         return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
+    }
+
+    @Bean
+    public GoogleAuthenticator googleAuthenticator(ICredentialRepository repo) {
+        GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder configBuilder
+                = new GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder();
+
+        configBuilder
+                .setTimeStepSizeInMillis(TimeUnit.SECONDS.toMillis(60))
+                .setWindowSize(10)
+                .setNumberOfScratchCodes(0);
+
+        GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator(configBuilder.build());
+        googleAuthenticator.setCredentialRepository(repo);
+        return googleAuthenticator;
     }
 
 }
--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -1,25 +1,33 @@
 package guru.sfg.brewery.web.controllers;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.seurity.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+@Slf4j
 @RequestMapping("/user/")
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserRepository userRepository;
+    private final GoogleAuthenticator googleAuthenticator;
 
     @GetMapping("register2fa")
     public String register2fa(Model model) {
-
-        model.addAttribute("googleurl", "todo");
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String url = GoogleAuthenticatorQRGenerator.getOtpAuthURL("L2BZ", user.getUsername(), googleAuthenticator.createCredentials());
+        log.debug(url);
+        model.addAttribute("googleurl", url);
 
         return "user/register2fa";
     }
